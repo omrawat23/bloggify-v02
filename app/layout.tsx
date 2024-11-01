5f1d06922b42ca243958a1e31c8d1b34b51c52fe
Om Rawat <omrawat23@gmail.com>
2024-11-01 19:22:10 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import './prosemirror.css'
-import { Providers } from "@/providers/Providers";
+import { Providers } from "@/providers/providers";
 import UserProvider from "@/providers/UserProvider";
 import { bric } from "@/utils/font";
 import Header from "@/components/Header";
